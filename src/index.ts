b3fb76b8984551c345e4b56a323acf34eee759b9
Paul Grenyer <paul.grenyer@gmail.com>
2023-04-30 12:55:07 +0100

    feat: added call to backend

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
+import axios from 'axios';
 
 const port = 8080;
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    // eslint-disable-next-line no-console
-    console.log(req);
-    res.send('Code Challenge!');
+app.get('/', async (req: Request, res: Response) => {
+    const response = await axios.get('http://localhost:8081');
+    res.send(response.data);
 });
 
 app.listen(port, () => {
